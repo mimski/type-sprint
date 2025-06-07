9d80c129d5280fe55a2fffbd1fffa9950fe802ae
mimski <maria.bg.1989@gmail.com>
2025-06-07 14:26:12 +0300

    added animation for the Resullts component with framer motion library

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const Results = ({
   accuracyPercentage,
   errors,
@@ -9,15 +11,46 @@ const Results = ({
   total: number;
   className?: string;
 }) => {
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+
   return (
-    <ul
+    <motion.ul
+      initial={initial}
+      animate={animate}
       className={`flex flex-col items-center text-yellow-400 space-y-3 ${className}`}
     >
-      <li className="text-xl font-semibold">Results</li>
-      <li>Accuracy: {accuracyPercentage}%</li>
-      <li className="text-red-500">Errors: {errors}</li>
-      <li>Typed: {total}</li>
-    </ul>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3 }}
+        className="text-xl font-semibold"
+      >
+        Results
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 0.5 }}
+      >
+        Accuracy: {accuracyPercentage}%
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1 }}
+        className="text-red-500"
+      >
+        Errors: {errors}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1.4 }}
+      >
+        Typed: {total}
+      </motion.li>
+    </motion.ul>
   );
 };
 
