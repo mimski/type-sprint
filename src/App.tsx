c15b7069572ae5a44e2047feb19d43c7a3efa950
mimski <maria.bg.1989@gmail.com>
2025-06-07 13:32:21 +0300

    used faker library to generate 10 random words

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { faker } from "@faker-js/faker";
+
+const words = faker.word.words(10);
 
 function App() {
   return (
     <>
-      <div className="text-4xl text-center text-yellow-500">Hello world!</div>
+      <div className="text-4xl text-center text-slate-500">{words}</div>
     </>
   );
 }
